5b4c9c3f462e896d547ccd77e5f2c9176830c2f6
Dilenio Enderle <dilenio@gmail.com>
2020-09-18 16:58:47 -0300

    Renderize um formulário dentro de AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor() {
+    super()
+
+    this.renderTitle = this.renderTitle.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+  }
+
+  handleChange({ target }) {
+    this.setState(() => ({ [target.name]: target.value }))
+  }
+
+  renderTitle() {
+    return (
+      <label htmlFor="" data-testid="title-input-label">
+        Título
+        <input name="title" type="text" data-testid="title-input" value={this.state.title} onChange={this.handleChange} />
+      </label>
+    )
+  }
+
   render() {
     const { onClick } = this.props
     return <div>
       <form data-testid="add-movie-form">
+        {this.renderTitle()}
       </form>
     </div>;
   }
